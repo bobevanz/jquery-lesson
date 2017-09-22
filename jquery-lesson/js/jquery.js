076068c2eafa5887e749668ee713a41377dc6ff7
Bob <bobevans547@gmail.com>
2017-09-22 15:29:58 -0400

    add jquery-lesson

--- jquery-lesson/js/jquery.js
+++ jquery-lesson/js/jquery.js
@@ -1,9 +1,22 @@
 $().ready(function(){
+	var InputTextIsRed = !true;
 	console.log("jquery ready");
+
 	$("button").click(function(){
 		console.log("button clicked!");
-	
+		
+	// $("#buttonOut").val("Button click worked!");
+ 
+	var msg = $("#buttonOut").val();
 
-	});
+	$("#valueIn").val( msg);
 
-    });
+	if (InputTextIsRed) {
+		$("#valueIn").css("color", "red");}
+		 else{
+		$("#valueIn").css("color", "green");
+	}
+	InputTextIsRed = !InputTextIsRed;
+	$("ul").append("<li>" + msg + "</li>")
+ });
+});
