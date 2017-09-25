63ee97957264f140289277664fd9e767d7a49ce7
Bob <bobevans547@gmail.com>
2017-09-25 15:56:10 -0400

    added AjaxLesson

--- AjaxLesson/JS/Ajax-Lesson.js
+++ AjaxLesson/JS/Ajax-Lesson.js
@@ -1,12 +1,42 @@
-$().ready(function() {
-		$.getJSON(
-			" http://md5.jsontest.com/?text=This is the md5",
-			function(MD5){
-				console.log("My MD5 is", MD5["md5"]);
-				console.log("The original is", MD5["original"]);
+$().ready(function() { //This and the button, along with some html... this is how you normally select a specific vendor from list of vendors  
+	function getVendor(id){
+	$.getJSON(
+		"http://prs.gregorydoud.net/Vendors/Get/" + id, // http://prsng.shows front end
+		function(vendor) {
+			console.log(vendor);
 			}
-
 		);
+	} // end function
+
+$("button").click(function() { // see first comment
+	var vendorId = $("#inputId").val();
+	getVendor(vendorId);
+
+}); // end button function
+
+	// $.getJSON(
+	// 	"http://prs.gregorydoud.net/Vendors/List",
+	// 	function(vendors){
+	// 		console.log(vendors);
+	// 		processVendors(vendors);
+	// 	}
+	// 	);
+
+	// 	function processVendors(vendors){
+	// 		for(var vendor of vendors){
+	// 			console.log(vendor.Name, vendor.Email, vendor.Phone );
+	// 		}
+	// 	}
+
+
+// 		$.getJSON(
+// 			" http://md5.jsontest.com/?text=This_is_the_md5",
+// 			function(MD5){
+// 				console.log("My MD5 is", MD5["md5"]);
+// 				console.log("The original is", MD5["original"]);
+// 			}
+
+// 		);
 
 	// $.getJSON(
 	// 	 "http://headers.jsontest.com/",
@@ -24,5 +54,4 @@ $().ready(function() {
 
 	// console.log("This is after the console log for my ip");
 
-
-});
+}); // end of ready function
