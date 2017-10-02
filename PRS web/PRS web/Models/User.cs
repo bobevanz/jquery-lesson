b6da2a2d2647bede18e70b687cd0fda2b8a25379
Bob <bobevans547@gmail.com>
2017-10-02 10:17:01 -0400

    Added User class

--- PRS web/PRS web/Models/User.cs
+++ PRS web/PRS web/Models/User.cs
@@ -5,7 +5,16 @@ using System.Web;
 
 namespace PRS_web.Models
 {
-    public class User
-    {
+    public class User{
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsReviewer { get; set; }
+        public bool IsAdmin { get; set; }
     }
-}
+    }
+    
